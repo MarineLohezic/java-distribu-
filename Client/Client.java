a479d229140c3a5c1356cce10325219c4484f7b2
MarineLohezic <lohezic.marine@gmail.com>
2017-07-27 18:39:05 +0200

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -1,5 +1,5 @@
 //----------
-// Marine Lohezic & Helene Levry 
+// Marine Lohezic
 //20/03/2017
 //----------
 
@@ -25,25 +25,25 @@ public class Client{
    public static void main(String[] args) {   
      
       try {
-      // Obtenir une référence du registry distant
-      // le port par défaut utilisé par le processus rmiregistery est le port 1099
+      // Obtenir une rÃ©fÃ©rence du registry distant
+      // le port par dÃ©faut utilisÃ© par le processus rmiregistery est le port 1099
          Registry registry = LocateRegistry.getRegistry("172.17.3.191");
       
-      // Obtenir une référence de l'objet distant
+      // Obtenir une rÃ©fÃ©rence de l'objet distant
          Anagramme objetd = (Anagramme) registry.lookup("monObjetDistant");
          
          Scanner sc = new Scanner(System.in);
          System.out.println("VEUILLEZ SAISIR UN MOT :");
          String mot = sc.nextLine(); // saisi du mot par l'utilisateur
-         System.out.println(mot); // verification du mot entrée
+         System.out.println(mot); // verification du mot entrÃ©e
          
-         int nbAna = objetd.countAnagram(mot); // appel la methode coté serveur
+         int nbAna = objetd.countAnagram(mot); // appel la methode cotÃ© serveur
          System.out.println("Nombre d'anagrammes du mot " + mot+ " : " + nbAna);
          
          String[] Anagrammes = objetd.getAnagram(); // appel a la methode
       
          for (int i = 0; i<Anagrammes.length; i++){
-            System.out.println(Anagrammes[i]); // affichage des anagrammes trouvés par le serveur
+            System.out.println(Anagrammes[i]); // affichage des anagrammes trouvÃ©s par le serveur
          } 
          
       } 
